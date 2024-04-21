ea8d6ded82d120a1068112f56242320b4d6002e2
swoocn <swooshinn@gmail.com>
2024-04-20 23:41:26 -0400

    Configure prod asset directory /dist; port Map of Pi favicons.

---
+++ dist/map-of-pi/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"IfhfuGVfP9l9j4hTaDaXIJtJ7fXLiQnf5KmjUC/E3CA=\"}"
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  distDir: 'dist/map-of-pi',
+}
 
 export default nextConfig;
