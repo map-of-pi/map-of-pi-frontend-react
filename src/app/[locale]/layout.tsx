1c65c6db1204cb47eedf7b850e7a5d50231d4eff
soleil00 <srukundo01@gmail.com>
2024-05-04 20:18:00 +0200

    added leaflet map with dummy markers

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -11,7 +11,70 @@ export default function LocaleLayout({
 }) {
   return (
     <html lang={locale} suppressHydrationWarning={true}>
-      <body className='bg-white dark:bgwhite'>
+      <head>
+        <meta charSet="utf-8" />
+        <title>Map of Pi</title>
+        <base href="/" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <meta
+          property="og:title"
+          content="Map of Pi, Searchable places accepting Pi on a map"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://mapofpi.concretecode.ch" />
+        <meta
+          property="og:image"
+          content="https://mapofpi.concretecode.ch/assets/images/logo.svg"
+        />
+        <meta
+          name="description"
+          content="Map of Pi is a mobile application developed to help Pi community members easily locate local businesses that accept Pi as payment"
+        />
+        <meta name="keywords" content="map, pi, business, app" />
+        <meta name="author" content="Map of Pi Team" />
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="font-src 'self' https://cdnjs.cloudflare.com/ajax/libs/font-awesome/https://fonts.gstatic.com/;"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.2.0/dist/leaflet.css"
+        />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      </head>
+      <body className="bg-white dark:bgwhite flex flex-col min-h-screen relative overflow-hidden">
         {/* <AppWrapper> */}
         <Providers>
           <Navbar />
---
+++ src/components/mappopup/MapPopupCard.tsx
@@ -0,0 +1,78 @@
+import { Popup } from 'react-leaflet';
+
+const MapPopupCard = () => {
+  return (
+    <Popup>
+      {/* <MapPopupCard shop={coord} /> */}
+      <div className="max-w-sm rounded-md overflow-hidden">
+        <img
+          className="w-full"
+          src="https://tse3.mm.bing.net/th?id=OIP.Ijh2DJJVlAmhBAYABFwI-wAAAA&pid=Api&P=0&h=220"
+          alt="Shop Image"
+        />
+        <div className="flex items-center justify-between my-2">
+          <div className="font-bold text-md ">Shop name</div>
+        </div>
+        <div className="space-y-1">
+          <div className="flex items-center">
+            <svg
+              className="h-6 w-6 flex-none fill-gray-100 stroke-green-500 stroke-2"
+              stroke-linecap="round"
+              stroke-linejoin="round">
+              <circle cx="12" cy="12" r="11" />
+              <path
+                d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9"
+                fill="none"
+              />
+            </svg>
+            <div className="ml-1">
+              <code className="text-sm font-bold text-gray-900">XXX km</code> 34
+              km
+            </div>
+          </div>
+          <div className="flex items-center">
+            <svg
+              className="h-6 w-6 flex-none fill-gray-100 stroke-green-500 stroke-2"
+              stroke-linecap="round"
+              stroke-linejoin="round">
+              <circle cx="12" cy="12" r="11" />
+              <path
+                d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9"
+                fill="none"
+              />
+            </svg>
+            <div className="ml-1">accept online payment</div>
+          </div>
+          <div className="flex items-center">
+            <svg
+              className="h-6 w-6 flex-none fill-gray-100 stroke-green-500 stroke-2"
+              stroke-linecap="round"
+              stroke-linejoin="round">
+              <circle cx="12" cy="12" r="11" />
+              <path
+                d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9"
+                fill="none"
+              />
+            </svg>
+            <div className="ml-1">34 products</div>
+          </div>
+        </div>
+
+        <div className="flex items-center mt-3 justify-center gap-3">
+          <div
+            className="bg-red-800 w-[100px]   text-center rounded-md text-white py-1 mt-1"
+            id="${shop._id}">
+            visit shop
+          </div>
+          <div
+            className="bg-blue-800 w-[100px]   text-center rounded-md text-white py-1 mt-1"
+            id="comming">
+            take routes
+          </div>
+        </div>
+      </div>
+    </Popup>
+  );
+};
+
+export default MapPopupCard;
