35843dd8dcd475d6321134c5c5f9bee0ac714a83
swoocn <swooshinn@gmail.com>
2024-05-27 10:49:35 -0400

    Make sidebar fully scrollable.

--- src/components/shared/sidebar/sidebar.tsx
+++ src/components/shared/sidebar/sidebar.tsx
@@ -150,34 +150,31 @@ function Sidebar(props: any) {
                   </div>
                   {/* MENU WITH CHILDREN */}
                   {menu.children && toggle[menu.title] &&
-                    <div className={menu.title === 'Languages' ? styles.scrollable_section : ''}>
-                      {menu.children.map((child) => (
-                        <div key={child.id} className="ml-6">
-                          <div
-                            className={`${styles.slide_content} hover:bg-[#424242] hover:text-white hover:dark:bg-[#ffffff] dark:text-white hover:dark:text-black`}
-                            onClick={() => handleChildMenu(menu.title, child.code)}>
-                            {child.icon && (  // conditional rendering
-                              <Image
-                                src={child.icon}
-                                alt={child.title}
-                                width={17}
-                                height={17}
-                                className=""
-                              />
-                            )}
-                            {menu.title === 'Languages' && isLanguageMenuItem(child) ? (
-                              <div className="ml-2 text-[14px]">
-                                <div className="font-bold">{child.label}</div>
-                                <div>{child.translation}</div>
-                              </div>
-                            ) : (
-                              <span className="ml-2 text-[14px]">{child.title}</span>
-                            )}
-                          </div>
+                    menu.children.map((child) => (
+                      <div key={child.id} className="ml-6">
+                        <div
+                          className={`${styles.slide_content} hover:bg-[#424242] hover:text-white hover:dark:bg-[#ffffff] dark:text-white hover:dark:text-black`}
+                          onClick={() => handleChildMenu(menu.title, child.code)}>
+                          {child.icon && (  // conditional rendering
+                            <Image
+                              src={child.icon}
+                              alt={child.title}
+                              width={17}
+                              height={17}
+                              className=""
+                            />
+                          )}
+                          {menu.title === 'Languages' && isLanguageMenuItem(child) ? (
+                            <div className="ml-2 text-[14px]">
+                              <div className="font-bold">{child.label}</div>
+                              <div>{child.translation}</div>
+                            </div>
+                          ) : (
+                            <span className="ml-2 text-[14px]">{child.title}</span>
+                          )}
                         </div>
-                      ))}
-                    </div>
-                  }
+                      </div>
+                    ))}
                 </div>
               </>
             ))}
