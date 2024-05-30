9971e365c634bc6ed24bd00a495e499e027c95e2
francis <frankdevlopes@gmail.com>
2024-05-30 15:29:43 +0300

    Added/Edit seller registration feature

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,13 +1,14 @@
 'use client';
 
-import { Button, YellowBtn } from '@/components/shared/Forms/Buttons/Buttons';
+import { Button } from '@/components/shared/Forms/Buttons/Buttons';
 import SearchBar from '@/components/shared/SearchBar/SearchBar';
 import dynamic from 'next/dynamic';
 import Image from 'next/image';
+import Link from 'next/link';
 import { FaCirclePlus } from "react-icons/fa6";
 
-export default function Index({}: { params: { locale: string } }) {
-  const DynamicMap = dynamic(() => import('../../components/shared/map/Map'), {
+export default function Index() {
+  const DynamicMap = dynamic(() => import('@/components/shared/map/Map'), {
     ssr: false,
   });
 
@@ -16,15 +17,17 @@ export default function Index({}: { params: { locale: string } }) {
       <DynamicMap />
       <SearchBar />
       <div className="absolute bottom-8 z-10 flex justify-between gap-[22px] px-7 right-0 left-0 m-auto">
+        <Link href="/seller-registration">
           <Button
             label="+ Sell"
             styles={{ borderRadius: '10px', color: '#ffc153', paddingLeft: '50px', paddingRight: '50px' }}
           />
-          <Button
-            icon={<Image src='/images/shared/my_location.png' width={30} height={30} alt='my location' objectFit='cover' />}
-            styles={{ borderRadius: '50%', width: '40px', height: '40px', padding: '0px' }}
-          />
-        </div>
+        </Link>
+        <Button
+          icon={<Image src='/images/shared/my_location.png' width={30} height={30} alt='my location' />}
+          styles={{ borderRadius: '50%', width: '40px', height: '40px', padding: '0px' }}
+        />
+      </div>
     </>
   );
 }
