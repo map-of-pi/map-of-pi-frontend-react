51ff5ce4f76a82aaf8f635853b8352e51095afc1
Darin Hajou <darinhajou@gmail.com>
2024-06-07 15:34:49 +0200

    Integrate leaflet for map functionality

---
+++ src/app/_document.tsx
@@ -0,0 +1,18 @@
+// pages/_document.tsx or the equivalent in your app structure
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
