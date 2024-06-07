6f36e9b06f795587301f6d102c0d29cf7a09f169
Darin Hajou <darinhajou@gmail.com>
2024-06-07 15:27:37 +0200

    Add content to page.tsx

---
+++ src/app/[locale]/center-map/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CenterMap from '../../../components/shared/map/CenterMap';
+
+const CenterMapPage = () => {
+  return (
+    <div>
+      <h1>Set Map Center</h1>
+      <CenterMap />
+    </div>
+  );
+};
+
+export default CenterMapPage;
