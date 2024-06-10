c8ddef6a4b947686038ae5192eae651fac87939e
Danny Lee <72060658+swoocn@users.noreply.github.com>
2024-06-10 14:04:59 -0400

    Add Language Translation. (#71)
    * Build EN translation file.
    
    * Add translation markers for 'Home' screen.
    
    * Add translation markers for 'Map Marker' popup.
    
    * Add translation markers for 'User Preferences' side navigation bar.
    
    * Add translation markers for 'Seller Reviews' screen.
    
    * Add translation markers for 'Reply to Review' screen.
    
    * Add translation markers for 'App Version' popup.
    
    * Add translation markers for 'Privacy Policy' popup.
    
    * Add translation markers for 'Terms of Service' popup.
    
    * Add translation markers for 'Buy from Seller' screen.
    
    * Add translation markers for 'Seller Registration' screen.
    
    * Build ES translation file.
    
    * Build KO translation file.

--- src/app/[locale]/seller/seller-item/page.tsx
+++ src/app/[locale]/seller/seller-item/page.tsx
@@ -1,20 +1,21 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
-
 import { useTranslations } from 'next-intl';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+
+import { useEffect, useState } from 'react';
 
 import TrustMeter from '@/components/seller/TrustMeter';
 import EmojiPicker from '@/components/shared/Review/emojipicker';
 import { OutlineBtn } from '@/components/shared/Forms/Buttons/Buttons';
 import { FileInput, TextArea } from '@/components/shared/Forms/Inputs/Inputs';
 import ConfirmDialog from '@/components/shared/confirm';
-
 import { itemData } from '@/constants/demoAPI';
 
 export default function Page() {
+  const SUBHEADER = "font-bold mb-2";
+
   const t = useTranslations();
   const router = useRouter();
 
@@ -24,7 +25,6 @@ export default function Page() {
   const [reviewEmoji, setReviewEmoji] = useState(null);
   const [isSaveEnabled, setIsSaveEnabled] = useState(false);
   const [showConfirmDialog, setShowConfirmDialog] = useState(false);
-  // const [dialogueMessage, setDialogueMessage] = useState('')
   const [linkUrl, setLinkUrl] = useState('');
 
   useEffect(() => {
@@ -78,11 +78,20 @@ export default function Page() {
     }
   };
 
-  const SUBHEADER = "font-bold mb-2";
+  const translateSellerCategory = (category: string): string => {
+    switch (category) {
+      case 'Pioneer':
+        return t('SCREEN.SELLER_REGISTRATION.SELLER_TYPE.SELLER_TYPE_OPTIONS.PIONEER');
+      case 'Other':
+        return t('SCREEN.SELLER_REGISTRATION.SELLER_TYPE.SELLER_TYPE_OPTIONS.OTHER');
+      default:
+        return category;
+    }
+  };
 
   return (
     <div className="w-full md:w-[500px] md:mx-auto p-4">
-      <h1 className="mb-5 font-bold text-lg md:text-2xl">Buy From Seller</h1>
+      <h1 className="mb-5 font-bold text-lg md:text-2xl">{t('SCREEN.BUY_FROM_SELLER.BUY_FROM_SELLER_HEADER')}</h1>
 
       {/* Seller Profile */}
       <div className="flex gap-4 align-center mb-6 relative">
@@ -91,18 +100,18 @@ export default function Page() {
         </div>
         <div className="my-auto">
           <h2 className="font-bold mb-2">{itemData.seller.business}</h2>
-          <p className="text-sm">{itemData.seller.category}</p>
+          <p className="text-sm">{translateSellerCategory(itemData.seller.category)}</p>
         </div>
       </div>
 
       {/* Seller Description */}
       <div className="mb-5">
-        <h2 className={SUBHEADER}>Seller Description</h2>
+        <h2 className={SUBHEADER}>{t('SCREEN.BUY_FROM_SELLER.SELLER_DESCRIPTION_LABEL')}</h2>
         <p className="">{itemData.seller.description}</p>
       </div>
 
       {/* Items List */}
-      <h2 className={SUBHEADER}>Seller items for sale</h2>
+      <h2 className={SUBHEADER}>{t('SCREEN.BUY_FROM_SELLER.SELLER_SALE_ITEMS_LABEL')}</h2>
       <div className="seller_item_container mb-6">
         <ul>
           {itemData.items.map((item) => (
@@ -116,24 +125,24 @@ export default function Page() {
 
       {/* Seller Location */}
       <div className="mb-6">
-        <h2 className={`SUBHEADER`}>Seller address or whereabouts</h2>
+        <h2 className={`SUBHEADER`}>{t('SCREEN.BUY_FROM_SELLER.SELLER_ADDRESS_LOCATION_LABEL')}</h2>
         <p className="mb-3">{itemData.seller.address}</p>
         <OutlineBtn
-          label="Navigate"
+          label={t('SHARED.NAVIGATE')}
           onClick={() => handleNavigation('location')}
         />
       </div>
 
       {/* Leave a Review */}
       <div className="mb-3">
-        <h2 className={SUBHEADER}>Leave a review</h2>
-        <p>Select the face which shows how you feel about the above Seller</p>
+        <h2 className={SUBHEADER}>{t('SCREEN.BUY_FROM_SELLER.LEAVE_A_REVIEW_MESSAGE')}</h2>
+        <p>{t('SCREEN.BUY_FROM_SELLER.FACE_SELECTION_REVIEW_MESSAGE')}</p>
         <EmojiPicker onSelect={handleEmojiSelect} />
       </div>
 
       <div className="mb-2">
         <TextArea
-          placeholder={'Enter additional comments here...'}
+          placeholder={t('SCREEN.BUY_FROM_SELLER.ADDITIONAL_COMMENTS_PLACEHOLDER')}
           value={comments}
           onChange={handleCommentsChange}
         />
@@ -141,7 +150,7 @@ export default function Page() {
 
       <div className="mb-2">
         <FileInput
-          label={'Optional feedback photo upload'}
+          label={t('SCREEN.BUY_FROM_SELLER.FEEDBACK_PHOTO_UPLOAD_LABEL')}
           handleAddImages={handleAddImages}
           images={previewImage}
         />
@@ -153,44 +162,39 @@ export default function Page() {
           onClick={handleSave}
           disabled={!isSaveEnabled}
           className={`${isSaveEnabled ? 'opacity-100' : 'opacity-50'} px-6 py-2 bg-primary text-white text-xl rounded-md flex justify-right ms-auto text-[15px]`}>
-          Save
+          {t('SHARED.SAVE')}
         </button>
       </div>
 
       {/* Summary of Reviews */}
       <div className="mb-7">
-        <h2 className={SUBHEADER}>Reviews summary</h2>
+        <h2 className={SUBHEADER}>{t('SCREEN.BUY_FROM_SELLER.REVIEWS_SUMMARY_LABEL')}</h2>
         {/* Trust-O-meter */}
         <div>
           <TrustMeter ratings={itemData.seller.ratings} />
         </div>
         <div className="flex items-center justify-between mt-3">
           <p className="text-sm">
-            Reviews Score: {itemData.seller.ratings} out of 5.0
+            {t('SCREEN.BUY_FROM_SELLER.REVIEWS_SCORE_MESSAGE', {seller_review_rating: itemData.seller.ratings})}
           </p>
-          {/* <button className="outline outline-[#8DBE95] hover:bg-[#386F4F] hover:text-white text-xl text-yellow-500 py-2 px-4 rounded-md flex justify-right ms-auto"
-                        onClick={()=>handleNavigation('seller/seller-reviews')}
-                    >
-                        Check Reviews
-                    </button> */}
           <OutlineBtn
-            label="Check Reviews"
+            label={t('SHARED.CHECK_REVIEWS')}
             onClick={() => handleNavigation('seller/seller-reviews')}
           />
         </div>
       </div>
       <div className="mb-7">
-        <h2 className={`${SUBHEADER} mb-4`}>Seller contact details</h2>
+        <h2 className={`${SUBHEADER} mb-4`}>{t('SCREEN.BUY_FROM_SELLER.SELLER_CONTACT_DETAILS_LABEL')}</h2>
         <div className="text-sm mb-3">
-          <span className=" font-bold">Seller pioneer id: </span>
+          <span className=" font-bold">{t('SCREEN.BUY_FROM_SELLER.SELLER_PI_ID_LABEL') + ": "}</span>
           <span>{itemData.seller.pioneer_id}</span>
         </div>
         <div className="text-sm mb-3">
-          <span className=" font-bold">Seller phone: </span>
+          <span className=" font-bold">{t('SCREEN.BUY_FROM_SELLER.SELLER_PHONE_LABEL') + ": "}</span>
           <span>{itemData.seller.phone}</span>
         </div>
         <div className="text-sm mb-3">
-          <span className=" font-bold">Seller email: </span>
+          <span className=" font-bold">{t('SCREEN.BUY_FROM_SELLER.SELLER_EMAIL_LABEL') + ": "}</span>
           <span>{itemData.seller.email}</span>
         </div>
       </div>
@@ -198,7 +202,7 @@ export default function Page() {
         show={showConfirmDialog}
         onClose={() => setShowConfirmDialog(false)}
         onConfirm={setShowConfirmDialog}
-        message="You have unsaved changes. Do you really want to leave?"
+        message={t('SHARED.CONFIRM_DIALOG')}
         url={linkUrl} 
       />
     </div>
--- src/components/seller/add-edit-seller/SellerRegistration.tsx
+++ src/components/seller/add-edit-seller/SellerRegistration.tsx
@@ -1,8 +1,11 @@
 "use client";
 
-import React, { useState, useEffect } from 'react';
-import Image from 'next/image';
+import { useTranslations } from 'next-intl';
 import Link from 'next/link';
+
+import { useState, useEffect } from 'react';
+
+import { FileInput } from '@/components/shared/Forms/Inputs/Inputs';
 
 const INPUT_CLASS = 'w-full p-2 border rounded-lg';
 const BUTTON_CLASS = 'w-full p-2 rounded-lg';
@@ -19,6 +22,8 @@ const SellerRegistrationForm = () => {
     sellerAddress: ''
   });
   
+  const t = useTranslations();
+
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [isFormValid, setIsFormValid] = useState(false);
 
@@ -32,6 +37,8 @@ const SellerRegistrationForm = () => {
     setFormData({ ...formData, [name]: value });
   };
 
+  const handleAddImages = () => {};
+
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
       setSelectedFile(e.target.files[0]);
@@ -40,22 +47,21 @@ const SellerRegistrationForm = () => {
 
   return (
     <>
-    {/* <div className="p-4 max-w-md mx-auto bg-zinc-100 rounded-lg shadow-md h-screen"> */}
     <div className="p-4 max-w-md mx-auto ">
-      <h2 className="text-lg font-semibold mb-4">Add/Edit Seller Registration</h2>
+      <h2 className="text-lg font-semibold mb-4">{t('SCREEN.SELLER_REGISTRATION.SELLER_REGISTRATION_HEADER')}</h2>
       <div className="mb-4">
-        <label className={LABEL_CLASS}>Seller items for sale</label>
+        <label className={LABEL_CLASS}>{t('SCREEN.SELLER_REGISTRATION.SELLER_SALE_ITEMS_LABEL')}</label>
         <textarea
           name="itemsForSale"
           className={TEXTAREA_CLASS}
-          placeholder="Describe your items for sale, price, etc"
+          placeholder={t('SCREEN.SELLER_REGISTRATION.SELLER_SALE_ITEMS_PLACEHOLDER')}
           value={formData.itemsForSale}
           onChange={handleChange}
         ></textarea>
       </div>
-      <button className={`${BUTTON_CLASS} bg-green-600 text-white mb-4`}>Set Search Centre</button>
+      <button className={`${BUTTON_CLASS} bg-green-600 text-white mb-4`}>{t('SHARED.SEARCH_CENTER')}</button>
       <div className="mb-4">
-        <h3 className="text-sm font-semibold mb-2">Reviews Summary</h3>
+        <h3 className="text-sm font-semibold mb-2">{t('SCREEN.SELLER_REGISTRATION.REVIEWS_SUMMARY_LABEL')}</h3>
         <label className={LABEL_CLASS}>Trust-o-meter</label>
         <input type="range" className="w-full mb-2" min="0" max="100" value="50" />
         <div className="flex justify-between text-xs">
@@ -67,92 +73,79 @@ const SellerRegistrationForm = () => {
         </div>
       </div>
       <div className="mb-4">
-        <label className={LABEL_CLASS}>Reviews Score </label>
+        <label className={LABEL_CLASS}>{t('SCREEN.SELLER_REGISTRATION.REVIEWS_SCORE_LABEL')}</label>
         <Link href="/seller/seller-reviews">
          <button className={`${BUTTON_CLASS} border border-green-600 text-green-600`}>
-          Check Reviews
+          {t('SHARED.CHECK_REVIEWS')}
          </button>
         </Link>
       </div>
       <div className="mb-4">
-        <label className={LABEL_CLASS}>Seller Name</label>
+        <label className={LABEL_CLASS}>{t('SCREEN.SELLER_REGISTRATION.SELLER_NAME')}</label>
         <input
           type="text"
           name="sellerName"
           className={INPUT_CLASS}
-          placeholder="Seller Name"
+          placeholder={t('SCREEN.SELLER_REGISTRATION.SELLER_NAME')}
           value={formData.sellerName}
           onChange={handleChange}
         />
       </div>
       <div className="mb-4">
-        <label className={LABEL_CLASS}>Seller Type</label>
+        <label className={LABEL_CLASS}>{t('SCREEN.SELLER_REGISTRATION.SELLER_TYPE.SELLER_TYPE_LABEL')}</label>
         <select
           name="sellerType"
           className={INPUT_CLASS}
           value={formData.sellerType}
           onChange={handleChange}
         >
-          <option>Pioneer</option>
-          <option>Others</option>
+          <option>{t('SCREEN.SELLER_REGISTRATION.SELLER_TYPE.SELLER_TYPE_OPTIONS.PIONEER')}</option>
+          <option>{t('SCREEN.SELLER_REGISTRATION.SELLER_TYPE.SELLER_TYPE_OPTIONS.OTHER')}</option>
         </select>
       </div>
       <div className="mb-4">
-        <label className={LABEL_CLASS}>Seller business name</label>
+        <label className={LABEL_CLASS}>{t('SCREEN.SELLER_REGISTRATION.SELLER_BUSINESS_NAME')}</label>
         <input
           type="text"
           name="businessName"
           className={INPUT_CLASS}
-          placeholder="Seller business name"
+          placeholder={t('SCREEN.SELLER_REGISTRATION.SELLER_BUSINESS_NAME')}
           value={formData.businessName}
           onChange={handleChange}
         />
       </div>
       <div className="mb-4">
-        <label className={LABEL_CLASS}>Seller Description</label>
+        <label className={LABEL_CLASS}>{t('SCREEN.SELLER_REGISTRATION.SELLER_DESCRIPTION')}</label>
         <textarea
           name="sellerDescription"
           className={TEXTAREA_CLASS}
-          placeholder="Seller Description"
+          placeholder={t('SCREEN.SELLER_REGISTRATION.SELLER_DESCRIPTION')}
           value={formData.sellerDescription}
           onChange={handleChange}
         ></textarea>
       </div>
       <div className="mb-4">
-        <label className={LABEL_CLASS}>Seller address or whereabouts</label>
+        <label className={LABEL_CLASS}>{t('SCREEN.SELLER_REGISTRATION.SELLER_ADDRESS_LOCATION_LABEL')}</label>
         <textarea
           name="sellerAddress"
           className={TEXTAREA_CLASS}
-          placeholder="Describe location where you are selling from"
+          placeholder={t('SCREEN.SELLER_REGISTRATION.SELLER_ADDRESS_LOCATION_PLACEHOLDER')}
           value={formData.sellerAddress}
           onChange={handleChange}
         ></textarea>
       </div>
       <div className="mb-4">
-        <label className={LABEL_CLASS}>Upload Photo (Optional)</label>
-        <label className="border-dashed border-2 border-zinc-400 p-4 rounded-lg flex flex-col items-center cursor-pointer">
-          <Image src="/images/shared/upload.png" alt="Upload" width={100} height={100} className="mb-2" />
-          <p className="text-sm text-zinc-600">Drop your image here, or browse</p>
-          <p className="text-xs text-zinc-400">Supports: PNG, JPG, JPEG, WEBP</p>
-          <p className="text-xs text-red-600">One Photo Only</p>
-          <input 
-            type="file" 
-            accept="image/*" 
-            className="hidden" 
-            onChange={handleFileChange} 
-          />
-        </label>
-        {selectedFile && (
-          <div className="mt-2">
-            <p className="text-sm text-zinc-600">Selected file: {selectedFile.name}</p>
-          </div>
-        )}
+        <FileInput
+          label={t('SHARED.PHOTO.UPLOAD_PHOTO_LABEL')}
+          images={[]}
+          handleAddImages={handleAddImages}
+        />
       </div>
       <button
         className={`${BUTTON_CLASS} bg-zinc-400 text-white`}
         disabled={!isFormValid}
       >
-        Save
+        {t('SHARED.SAVE')}
       </button>
     </div>
     </>
