87ed3514f6fe4bbe154c5599c8fe13fc386d8d87
Darin Hajou <darinhajou@gmail.com>
2024-06-10 22:10:06 +0200

    Remove h1 tag from  center-map/page

--- src/app/[locale]/center-map/page.tsx
+++ src/app/[locale]/center-map/page.tsx
@@ -4,7 +4,6 @@ import CenterMap from '../../../components/shared/map/CenterMap';
 const CenterMapPage = () => {
   return (
     <div>
-      <h1>Set Map Center</h1>
       <CenterMap />
     </div>
   );
