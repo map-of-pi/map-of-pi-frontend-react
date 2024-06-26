3c87354ab044df56b1bcf56ff0d64bbf3deb13b1
omotoshoayomikun <omotoshoayomikun@gmail.com>
2024-06-26 23:36:04 +0100

    Implement Skeleton preloader

---
+++ src/components/skeleton/Sidebar.tsx
@@ -0,0 +1,28 @@
+export const SkeletonSidebar = () => {
+  return (
+    <>
+    <div className="skeleton">
+      <div className="w-[70%] h-7 mb-5"></div>
+
+      <div className="w-[52%] h-5 mb-2"></div>
+      <div className="w-full h-10 mb-3 rounded-md"></div>
+
+      <div className="w-[52%] h-5 mb-2"></div>
+      <div className="w-full h-10 mb-4 rounded-md"></div>
+
+      <div className="w-full h-10 mb-2 rounded-md"></div>
+      <div className="w-full h-10 mb-2 rounded-md"></div>
+
+      <div className="w-[40%] h-5 mb-2"></div>
+      <div className="w-full h-[180px] flex flex-col justify-end py-3 items-center mb-2 rounded-md">
+        <div
+          className="w-[60%] h-3 mb-3"
+          style={{ backgroundColor: '#EBF1ED' }}></div>
+        <div
+          className="w-[50%] h-3 mb-3"
+          style={{ backgroundColor: '#EBF1ED' }}></div>
+      </div>
+    </div>
+    </>
+  );
+};
