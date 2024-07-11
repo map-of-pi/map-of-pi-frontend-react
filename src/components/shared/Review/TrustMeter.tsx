a6a7ba36c839df6e382677d60adc1cf1c65b3b8d
omotoshoayomikun <omotoshoayomikun@gmail.com>
2024-07-11 02:05:12 +0100

    Trust-o-meter formatting needs fixing

--- src/components/shared/Review/TrustMeter.tsx
+++ src/components/shared/Review/TrustMeter.tsx
@@ -11,19 +11,19 @@ const TrustMeter: React.FC<TrustMeterProps> = ({ ratings }) => {
   const average = 2.5; // this can be adjusted
   const condition = ratings < average
 
-  const lines = Array.from({ length: 9 }, (_, i) => i); // Generates an array [0, 1, 2, ..., 7]
+  const lines = Array.from({ length: 11 }, (_, i) => i); // Generates an array [0, 1, 2, ..., 7]
 
   return (
     <div className='w-full flex'>
-      <h3 className='font-bold text-sm text-nowrap pe-3'>Trust-o-meter</h3>
-      <div className='w-full'>
+      <h3 className='font-bold text-sm text-nowrap pe-2'>Trust-o-meter</h3>
+      <div className='flex-1'>
 
         <div className="flex w-full">          
           {/* ball */}
           <div className={`${condition ? 'bg-red-700' : 'bg-green-700'} rounded-full p-3 w-[5px] h-[5px]`}></div>
           
           {/* Primary indicator */}
-          <div className={`rounded-e-[9px] h-[10px] w-full my-auto -ms-1 ${condition ? 'bg-red-200' : 'bg-green-200'}`}>
+          <div className={`rounded-e-[9px] h-[10px] w-full my-auto -ms-1 ${condition ? 'bg-red-400' : 'bg-green-400'}`}>
             
             {/* Secondary indicator */}
             <div
@@ -32,17 +32,24 @@ const TrustMeter: React.FC<TrustMeterProps> = ({ ratings }) => {
             ></div>
           </div>
         </div>
-        <div className="mb-3 w-full ps-[30px]">
-          <div className="flex gap-[6%] overflow-hidden">
+        <div className="mb-3 w-full ps-[25px]">
+          <div className="flex justify-between px-1 overflow-hidden">
             {lines.map((_, index) => (
-              <div key={index} style={{ width: '6%', height: '15px', borderInline: 'solid 1px black' }}></div>
+              <div key={index} className='bg-black rounded-t rounded-b' style={{ width: '2px', height: '10px' }}></div>
             ))}
           </div>
-          <div className="w-full flex justify-between">
-            <span>0%</span>
-            <span className="ps-[30%]">50%</span>
-            <span className="ps-[12%]">80%</span>
-            <span className="ps-[6.5%] md:ps-[11%]">100%</span>
+          <div className="flex justify-between">
+            <span className='w-[9.09%] text-[14px]'>0<span className=' text-[12px]'>%</span></span>
+            <span className='w-[9.09%] text-[14px]'></span>
+            <span className='w-[9.09%] text-[14px]'></span>
+            <span className='w-[9.09%] text-[14px]'></span>
+            <span className='w-[9.09%] text-[14px]'></span>
+            <span className='w-[9.09%] text-[14px]'>50<span className=' text-[12px]'>%</span></span>
+            <span className='w-[9.09%] text-[14px]'></span>
+            <span className='w-[9.09%] text-[14px]'></span>
+            <span className='w-[9.09%] text-[14px]'>80<span className=' text-[12px]'>%</span></span>
+            <span className='w-[9.09%] text-[14px]'></span>
+            <span className='w-[9.09%] text-[14px]'>100<span className=' text-[12px]'>%</span></span>
           </div>
         </div>
       </div>
