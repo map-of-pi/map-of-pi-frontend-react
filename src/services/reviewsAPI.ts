223876c40ba922473aff37071833a5f73bed41d2
swoocn <swooshinn@gmail.com>
2024-07-23 07:45:26 -0400

    PR adjustment; refactor and cleanup.

--- src/services/reviewsAPI.ts
+++ src/services/reviewsAPI.ts
@@ -1,16 +1,9 @@
-import { IUser, ReviewFeedbackType } from '@/constants/types';
 import axiosClient from "@/config/client";
-import axios from 'axios';
-
-
-const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8001/api/v1'
-});
 
 // Fetch a single review for a seller
 export const fetchSingleReview = async (reviewID: string) => {
     try {
-      const response = await API.get(`/review-feedback/single/${reviewID}`);
+      const response = await axiosClient.get(`/review-feedback/single/${reviewID}`);
       return response.data;
     } catch (error) {
       console.error(`Error fetching review with ID ${reviewID}:`, error);
@@ -21,7 +14,7 @@ export const fetchSingleReview = async (reviewID: string) => {
   // Fetch reviews for a seller
   export const fetchReviews = async (sellerId:string) => {
     try {
-      const response = await API.get(`/review-feedback/${sellerId}`);
+      const response = await axiosClient.get(`/review-feedback/${sellerId}`);
       return response.data;
     } catch (error) {
       console.error(`Error fetching reviews for seller with ID ${sellerId}:`, error);
@@ -32,7 +25,7 @@ export const fetchSingleReview = async (reviewID: string) => {
   // Create a new review
   export const createReview = async (formData: FormData, token: string) => {
     try {
-      const response = await API.post('/review-feedback/add', formData, {
+      const response = await axiosClient.post('/review-feedback/add', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
           'Authorization': `Bearer ${token}`
@@ -48,7 +41,7 @@ export const fetchSingleReview = async (reviewID: string) => {
   // Update a review
   export const updateReview = async (reviewId:string, formData:FormData, authToken: string) => {
     try {
-      const response = await API.put(`/review-feedback/${reviewId}`, formData, {
+      const response = await axiosClient.put(`/review-feedback/${reviewId}`, formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
           'Authorization': `Bearer ${authToken}`,
