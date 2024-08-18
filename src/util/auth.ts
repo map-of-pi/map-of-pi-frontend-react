cc757912929681d8c205ae2fb6801c7a1af34451
Darin Hajou <darinhajou@gmail.com>
2024-08-18 13:37:47 +0200

    Re-added missing auth functions (autoSigninUser, PiAuthentication) to ensure MapCenter integration works properly

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -1,3 +1,39 @@
+import axios from 'axios';
+import axiosClient, { setAuthToken } from "@/config/client";
+
+// Auto sign-in function
+export const autoSigninUser = async () => {
+  try {
+    const token = localStorage.getItem('authToken');
+    console.log('Retrieved token:', token);
+    if (!token) {
+      throw new Error('No token found');
+    }
+
+    setAuthToken(token);
+
+    const response = await axiosClient.get('/users/me');
+    console.log('Login result from autoSigninUser: ', response.data);
+
+    return { ...response.data, token };
+  } catch (error: any) {
+    console.error("Error during auto sign-in: ", error.response?.data || error.message);
+    throw new Error(error.response?.data?.message || error.message);
+  }
+};
+
+// Function to verify pioneer access token from the Pi Network
+export const PiAuthentication = async (PioneerAccessToken: string) => {
+  const header = {
+    headers: {
+      Authorization: `Bearer ${PioneerAccessToken}`
+    }
+  };
+  const res = await axios.get('https://api.minepi.com/v2/me', header);
+  console.log('Pioneer data obtained from Pioneer authentication', res.data)
+  return res.data;
+}
+
 interface PaymentDTO {
   amount: number,
   user_uid: string,
