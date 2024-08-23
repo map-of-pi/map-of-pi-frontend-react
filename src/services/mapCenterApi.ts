4416437853c19f69fc03e92ce65f4df1776e1484
Darin Hajou <darinhajou@gmail.com>
2024-08-23 18:15:44 +0200

    Simplify mapCenter API by removing unused fetchMapCenter function

--- src/services/mapCenterApi.ts
+++ src/services/mapCenterApi.ts
@@ -1,26 +1,10 @@
 import axiosClient from "@/config/client";
 import { handleAxiosError } from "@/util/error";
 
-// Function to Fetch Map Center
-export const fetchMapCenter = async () => {
-  try {
-    const response = await axiosClient.get('/map-center');
-    if (response.status === 200) {
-      return response.data;
-    } else {
-      console.error(`Fetch map center failed: ${response.status}`);
-      return null;
-    }
-  } catch (error: any) {
-    handleAxiosError(error);
-    throw error;
-  }
-};
-
 // Function to Save Map Center
 export const saveMapCenter = async (latitude: number, longitude: number) => {
   try {
-    const response = await axiosClient.put(`/api/v1/map-center/save`, {
+    const response = await axiosClient.put(`/map-center/save`, {
       latitude,
       longitude
     });
