08f157013c03d0b1b2427206e0bb19c12e804525
swoocn <swooshinn@gmail.com>
2024-08-25 16:47:46 -0400

    Misc cleanup.

--- logger.config.mjs
+++ logger.config.mjs
@@ -3,17 +3,15 @@ import { logToSentry } from './sentry.client.config.mjs';
 
 export const configureLogger = () => {
   if (process.env.NODE_ENV === 'production') {
-    // Production: Log only errors and send them to Sentry
     log.setLevel('error');
 
     const originalErrorMethod = log.error;
     log.error = (...args) => {
-      originalErrorMethod(...args);  // Log the error to the console
-      logToSentry(args.join(' '));  // Send the error to Sentry
+      originalErrorMethod(...args);  // log the error to the console
+      logToSentry(args.join(' '));  // send the error to Sentry
     };
 
   } else if (process.env.NODE_ENV === 'development') {
-    // Development: Log at the info level to the console
     log.setLevel('info');
   }
 };
