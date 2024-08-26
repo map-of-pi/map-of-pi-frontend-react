c1c6148095f772a22f4348d1d890f2cdf0d1870a
Darin Hajou <darinhajou@gmail.com>
2024-08-26 20:46:20 +0200

    Replace console logs with logger for both saveMapCenter and fetchMapCenter

--- src/services/mapCenterApi.ts
+++ src/services/mapCenterApi.ts
@@ -1,38 +1,54 @@
 import axiosClient from "@/config/client";
 import { handleAxiosError } from "@/util/error";
+import logger from '../../logger.config.mjs';
 
 // Function to Save Map Center
 export const saveMapCenter = async (latitude: number, longitude: number) => {
   try {
-    const response = await axiosClient.put(`/map-center/save`, {
+    logger.info(`Saving map center with coordinates: latitude ${latitude}, longitude ${longitude}`);
+    const response = await axiosClient.put('/api/v1/map-center/save', {
       latitude,
       longitude
     });
+    
     if (response.status === 200) {
+      logger.info(`Save map center successful with Status ${response.status}`, {
+        data: response.data
+      });
       return response.data;
     } else {
-      console.error(`Save map center failed: ${response.status}`);
+      logger.error(`Save map center failed with Status ${response.status}`);
       return null;
     }
   } catch (error: any) {
+    logger.error(`Save map center encountered an error: ${error.message}`, {
+      error: error.toString()
+    });
     handleAxiosError(error);
     throw error;
   }
 };
 
 export const fetchMapCenter = async () => {
-  console.log("Fetching map center data...");
-  console.log()
   try {
-    const response = await axiosClient.get('/map-center');
+    logger.info('Fetching map center..');
+    const response = await axiosClient.get('/api/v1/map-center');
+    
     if (response.status === 200) {
+      logger.info(`Fetch map center successful with Status ${response.status}`, {
+        data: response.data
+      });
       return response.data;
     } else {
-      console.error(`Fetch map center failed: ${response.status}`);
+      logger.error(`Fetch map center failed with Status ${response.status}`);
       return null;
     }
   } catch (error: any) {
+    logger.error(`Fetch map center encountered an error: ${error.message}`, {
+      error: error.toString()
+    });
     handleAxiosError(error);
     throw error;
   }
 };
+
