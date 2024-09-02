7e56347e5da777cf7b5e227342a4b84666ec6fb2
Darin Hajou <darinhajou@gmail.com>
2024-09-02 19:43:16 +0200

    Declare leaflet-control-geocoder in type definitions

--- src/typedefs.d.ts
+++ src/typedefs.d.ts
@@ -1,3 +1,9 @@
 declare interface Window {
   Pi: any;
 }
+
+// Declaration for leaflet-control-geocoder
+declare module 'leaflet-control-geocoder/dist/Control.Geocoder.js' {
+  const Geocoder: any;
+  export default Geocoder;
+}
