69568c72f6c7798e7b6a009c30b57ac268682ce3
Darin Hajou <darinhajou@gmail.com>
2024-09-06 22:58:44 +0200

    Pass entryType as a prop in MapCenter page for dynamic label updates

--- src/app/[locale]/map-center/page.tsx
+++ src/app/[locale]/map-center/page.tsx
@@ -1,14 +1,20 @@
+'use client';
+
 import dynamic from 'next/dynamic';
+import { useSearchParams } from 'next/navigation';
 
 const MapCenterPage = () => {
+  const searchParams = useSearchParams();
+  const entryType = searchParams.get('entryType'); // Get 'entryType' from URL query params
 
+  // Dynamically import the MapCenter component
   const MapCenter = dynamic(() => import('@/components/shared/map/MapCenter'), {
     ssr: false,
-  })
+  });
 
   return (
     <>
-      <MapCenter />
+      <MapCenter entryType={entryType as 'search' | 'sell'} /> {/* Pass entryType as a prop */}
     </>
   );
 };
