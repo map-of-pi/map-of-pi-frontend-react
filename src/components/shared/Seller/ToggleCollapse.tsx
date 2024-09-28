7efba282b48e1915dce4f0bb55d90e92c45bc47b
Darin Hajou <darinhajou@gmail.com>
2024-09-29 00:45:05 +0200

    Refactor ToggleCollapse to support defaultOpen state

--- src/components/shared/Seller/ToggleCollapse.tsx
+++ src/components/shared/Seller/ToggleCollapse.tsx
@@ -1,22 +1,33 @@
 import React, { useState } from 'react';
 import { FaChevronDown } from 'react-icons/fa6';
 
-function ToggleCollapse({ children, header }: { children: React.ReactNode; header: string; }) {
-  const [toggle, setToggle] = useState(false);
+function ToggleCollapse({
+  children,
+  header,
+  defaultOpen = false,
+}: {
+  children: React.ReactNode;
+  header: string;
+  defaultOpen?: boolean;
+}) {
+  const [toggle, setToggle] = useState(defaultOpen);
+
   return (
-    <>
-      <div className="mb-2">
-        <div className="flex items-center gap-4 cursor-pointer mb-2" onClick={() => setToggle(!toggle)}>
-          <h2 className="font-bold">{header}</h2>
-          <FaChevronDown
-            size={13}
-            className={`text-[#000000] ${toggle && 'rotate-90'}`}
-          />
-        </div>
-        {toggle && children}
+    <div className="mb-2">
+      <div
+        className="flex items-center gap-4 cursor-pointer mb-2"
+        onClick={() => setToggle(!toggle)}
+      >
+        <h2 className="font-bold">{header}</h2>
+        <FaChevronDown
+          size={13}
+          className={`text-[#000000] ${toggle && 'rotate-90'}`}
+        />
       </div>
-    </>
+      {toggle && children}
+    </div>
   );
 }
 
 export default ToggleCollapse;
+
