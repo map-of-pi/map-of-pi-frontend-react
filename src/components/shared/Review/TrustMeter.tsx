19daa54609cded93a8255ed9aef1411ed1de5c92
francis <frankdevlopes@gmail.com>
2024-10-12 18:10:32 -0400

    removed-styles-props

--- src/components/shared/Review/TrustMeter.tsx
+++ src/components/shared/Review/TrustMeter.tsx
@@ -6,7 +6,7 @@ type TrustMeterProps = {
   hideLabel?: boolean;
 };
 
-const TrustMeter: React.FC<TrustMeterProps> = ({ ratings, hideLabel}) => {
+const TrustMeter: React.FC<TrustMeterProps> = ({ ratings, hideLabel }) => {
   const [ratingImage, setRatingImage] = useState<string>('')
 
   useEffect(() => {
@@ -16,20 +16,21 @@ const TrustMeter: React.FC<TrustMeterProps> = ({ ratings, hideLabel}) => {
       setRatingImage(`/images/shared/review_ratings/trust-o-meter_0${ratings}.PNG`);
     } else {
       setRatingImage(`/images/shared/review_ratings/trust-o-meter_${ratings}.PNG`);
-    }       
+    }
   }, [ratings]);
 
   return (
     <div className='w-full flex items-center mb-3'>
+      <h3 className={`font-bold text-sm text-nowrap pe-2 ${hideLabel ? 'hidden' : 'block'}`}></h3>
       <div className='flex-1'>
-        <div className="flex w-full">          
+        <div className="flex w-full">
           <Image 
             src={ratingImage} 
-            alt='100% ratings' 
+            alt='Rating Image' 
             width={400} 
             height={3}
             className="w-full max-w-xs md:max-w-md lg:max-w-lg"
-          />          
+          />
         </div>
       </div>
     </div>
