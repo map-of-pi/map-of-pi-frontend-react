f9798972e47d897ec18d35c954338842257b453e
Darin Hajou <darinhajou@gmail.com>
2024-11-04 14:45:36 +0100

    Add locale to 'Check Reviews' link in Buy Seller Items screen

--- src/app/[locale]/seller/sale-items/[id]/page.tsx
+++ src/app/[locale]/seller/sale-items/[id]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useTranslations } from 'next-intl';
+import { useTranslations, useLocale } from 'next-intl';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -21,9 +21,8 @@ import logger from '../../../../../../logger.config.mjs';
 
 export default function BuyFromSellerForm({ params }: { params: { id: string } }) {
   const SUBHEADER = "font-bold mb-2";
-
   const t = useTranslations();
-
+  const locale = useLocale();
   const sellerId = params.id; 
 
   const [showConfirmDialog, setShowConfirmDialog] = useState(false);
@@ -154,7 +153,7 @@ export default function BuyFromSellerForm({ params }: { params: { id: string } }
             <p className="text-sm">
               {t('SCREEN.BUY_FROM_SELLER.REVIEWS_SCORE_MESSAGE', {seller_review_rating: sellerShopInfo.average_rating.$numberDecimal})}
             </p>
-            <Link href={`/seller/reviews/${sellerId}?buyer=true&user_name=${sellerInfo?.pi_username}`}>
+            <Link href={`/${locale}seller/reviews/${sellerId}?buyer=true&user_name=${sellerInfo?.pi_username}`}>
             <OutlineBtn label={t('SHARED.CHECK_REVIEWS')} />
             </Link>
           </div>
