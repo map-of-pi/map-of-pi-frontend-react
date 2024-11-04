a5aeb06957c3f4c93174507f0c882d5403541444
swoocn <swooshinn@gmail.com>
2024-11-04 18:59:11 -0500

    Added 'locale' to Registration screen.

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -20,6 +20,7 @@ import logger from '../../../logger.config.mjs';
 
 export default function Page({ params }: { params: { locale: string } }) {
   const t = useTranslations();
+  const { locale } = params;
   const DynamicMap = dynamic(() => import('@/components/shared/map/Map'), {
     ssr: false,
   });
@@ -136,7 +137,7 @@ export default function Page({ params }: { params: { locale: string } }) {
         <div className="w-[90%] lg:w-full lg:px-6 mx-auto flex items-center justify-between">
           {/* Add Seller Button */}
           <div className="pointer-events-auto">
-            <Link href={'/seller/registration'}>
+            <Link href={`/${locale}/seller/registration`}>
               <Button
                 label={'+ ' + t('HOME.ADD_SELLER')}
                 styles={{
