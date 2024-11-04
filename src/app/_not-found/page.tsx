c046b8be0f0fd465096808a8e5cf411ad4349524
Darin Hajou <darinhajou@gmail.com>
2024-11-04 21:19:33 +0100

    Create and add dynamic rendering to _not-found page to support next-intl and ensure 404 handling

---
+++ src/app/_not-found/page.tsx
@@ -0,0 +1,11 @@
+// app/_not-found/page.tsx
+export const dynamic = 'force-dynamic';
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </div>
+  );
+}
