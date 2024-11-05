ca50ee73b0acbdf7f37e253cc236a6007b29446a
swoocn <swooshinn@gmail.com>
2024-11-05 00:01:48 -0500

    Introduce Benin Fon language translation; refactor locales reference using DRY principle.

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,7 +2,16 @@ import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
 
 // Can be imported from a shared config
-const locales = ['en', 'en-GB', 'es', 'ar', 'ko', 'hau-NG', 'ewe-BJ'];
+export const locales = [
+  'ar', 
+  'en', 
+  'en-GB', 
+  'es', 
+  'ewe-BJ', 
+  'fon-BJ', 
+  'hau-NG', 
+  'ko'
+] as const;
 
 export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
