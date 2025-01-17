7183e6257e22a9582cafefb3b772a85a9122fabf
Darin Hajou <darinhajou@gmail.com>
2025-01-17 20:37:08 +0100

    Add membershipPage to handle routing

---
+++ src/app/[locale]/membership/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import MembershipScreen from "@/components/shared/membership/membershipScreen";
+
+interface MembershipPageProps {}
+
+const MembershipPage: React.FC<MembershipPageProps> = () => {
+  return <MembershipScreen />;
+};
+
+export default MembershipPage;
