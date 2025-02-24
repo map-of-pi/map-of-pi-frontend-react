79588a706014e2f8710e97741e412f3c037d002e
swoocn <swooshinn@gmail.com>
2025-02-24 07:43:50 -0500

    Resolve deprecated i18n request configuration setup for internationalization.

---
+++ i18n/i18n.ts
@@ -0,0 +1,19 @@
+// Can be imported from a shared config
+export const locales = [
+  'ar', 
+  'en', 
+  'en-GB', 
+  'es', 
+  'ewe-BJ', 
+  'fon-BJ',
+  'fr', 
+  'hau-NG',
+  'yor-NG',
+  'ja', 
+  'ko',
+  'vi',
+  'zh-CN',
+  'zh-TW'
+] as const;
+
+export const defaultLocale = 'en';
