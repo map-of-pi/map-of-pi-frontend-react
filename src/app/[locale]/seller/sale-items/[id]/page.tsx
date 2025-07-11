c5116f57bb565724248983824d91e6dc9f5d0db5
swoocn <swooshinn@gmail.com>
2025-07-11 02:05:20 -0400

    Misc PR adjustments; minor refactoring.

--- src/app/[locale]/seller/sale-items/[id]/page.tsx
+++ src/app/[locale]/seller/sale-items/[id]/page.tsx
@@ -248,26 +248,24 @@ export default function BuyFromSellerForm({ params }: { params: { id: string } }
             header={t('SCREEN.SELLER_REGISTRATION.SELLER_ONLINE_SHOPPING_ITEMS_LIST_LABEL')}
             open={false}>
             <div className="overflow-x-auto mb-7 mt-3 flex p-2 gap-x-5 w-full">
-              {dbSellerItems && dbSellerItems.length > 0 &&
-                dbSellerItems
-                  .filter(item => {
-                    const isSold = item.stock_level === StockLevelType.sold;
-                    const isExpired = item.expired_by && new Date(item.expired_by) < new Date();
-                    return !isSold && !isExpired;
-                  })
-                  .map(item => (
-                    <ListItem
-                      key={item._id}
-                      item={item}
-                      pickedItems={pickedItems}
-                      setPickedItems={setPickedItems}
-                      refCallback={handleShopItemRef}
-                      totalAmount={totalAmount}
-                      setTotalAmount={setTotalAmount}
-                    />
-                  ))
+              {dbSellerItems && dbSellerItems.length > 0 && dbSellerItems
+                .filter(item => {
+                  const isSold = item.stock_level === StockLevelType.sold;
+                  const isExpired = item.expired_by && new Date(item.expired_by) < new Date();
+                  return !isSold && !isExpired;
+                })
+                .map(item => (
+                  <ListItem
+                    key={item._id}
+                    item={item}
+                    pickedItems={pickedItems}
+                    setPickedItems={setPickedItems}
+                    refCallback={handleShopItemRef}
+                    totalAmount={totalAmount}
+                    setTotalAmount={setTotalAmount}
+                  />
+                ))
               }
-
             </div>
             <div>
               <h2 className={SUBHEADER}>{t('SCREEN.SELLER_REGISTRATION.FULFILLMENT_METHOD_TYPE.FULFILLMENT_METHOD_TYPE_LABEL')}</h2>
